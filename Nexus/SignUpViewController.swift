07f5d8e00c65d1a8cd3821c5e10ffe984c4d8408
nickpotts3 <32562264+nickpotts3@users.noreply.github.com>
2018-01-08 00:28:06 -0500

    Each user now takes up own spot on db. Email is stored to their db location

--- Nexus/SignUpViewController.swift
+++ Nexus/SignUpViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseDatabase
 
 class SignUpViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
@@ -18,15 +19,23 @@ class SignUpViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
+//sends email and password to firebase in order to create a new user
     @IBAction func SignUpBtn_TouchUpInside(_ sender: Any) {
         Auth.auth().createUser(withEmail: emailTextField.text! , password: passwordTextField.text! , completion: {
             (user: User?, error: Error?) in
-            if error != nil {
-                print(error?.localizedDescription)
+            if error != nil { //if an error occurs it will print to the console
+                print(error!.localizedDescription)
                 return
             }
-            print(user)
+            // All users are one giant node and each user is a child node
+            // reference a location in the database
+            let ref = Database.database().reference()
+            let usersReference = ref.child("users")
+            // print(usersReference.description()) prints out https://nexus-app-6f0eb.firebaseio.com/users
+            let uid = user?.uid // id is created by firebase upon account creation
+            let newUsersReference = usersReference.child(uid!) // points to new user on database
+            newUsersReference.setValue(["email": self.emailTextField.text!]) // self is the view controller need this process for other user info entered
+            print(newUsersReference.description())
         })
     }
 }
