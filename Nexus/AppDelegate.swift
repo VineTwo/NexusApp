fb830817474b41fdc025788f3b2c8f6edb878f14
nickpotts3 <32562264+nickpotts3@users.noreply.github.com>
2018-01-10 17:07:27 -0500

    Now it should merge

--- Nexus/AppDelegate.swift
+++ Nexus/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         // Override point for customization after application launch.
-        let navigationBarAppearace = UINavigationBar.appearance()
+        var navigationBarAppearace = UINavigationBar.appearance()
         
         navigationBarAppearace.tintColor = UIColor(red: 85/255, green: 204/255, blue: 238/255, alpha: 1.0) /* #55ccee */
         navigationBarAppearace.barTintColor = UIColor(red: 85/255, green: 204/255, blue: 238/255, alpha: 1.0) /* #55ccee */
