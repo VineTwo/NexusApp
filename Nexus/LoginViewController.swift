1de5ea24b699385f6775b3fd79588bae0801704b
nickpotts3 <32562264+nickpotts3@users.noreply.github.com>
2018-01-10 23:15:42 -0500

    When signing in, it authenticates email with firebase.

--- Nexus/LoginViewController.swift
+++ Nexus/LoginViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     let textFieldColor = UIColor(red: 0, green: 0.1, blue: 0.5, alpha: 0.2)
     
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +27,32 @@ class LoginViewController: UIViewController {
         passwordTextField.tintColor = UIColor.gray
         passwordTextField.textColor = UIColor.black
         passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSAttributedStringKey.foregroundColor: UIColor(white: 1.0, alpha: 0.9)])
+        loginButton.isEnabled = false
+        loginButton.setTitleColor(UIColor.lightText, for: UIControlState.normal)
+        handleTextField()
     }
+    func handleTextField() {
+        userNameTextField.addTarget(self, action: #selector(SignUpViewController.textFieldDidChange), for: UIControlEvents.editingChanged)
+        passwordTextField.addTarget(self, action: #selector(SignUpViewController.textFieldDidChange), for: UIControlEvents.editingChanged)
+    }
+    @objc func textFieldDidChange() {
+        guard let username = userNameTextField.text, !username.isEmpty, let password = passwordTextField.text, !password.isEmpty else {
+            loginButton.setTitleColor(UIColor.lightText, for: UIControlState.normal)
+            return
+        }
+        loginButton.setTitleColor(UIColor.black, for: UIControlState.normal)
+        loginButton.isEnabled = true
+    }
+    
+    @IBAction func signInButton_TouchUpInside(_ sender: Any) {
+        Auth.auth().signIn(withEmail: userNameTextField.text!, password: passwordTextField.text!, completion: { (user, Error) in
+            if Error != nil {
+                print(Error!.localizedDescription)
+                return
+            }
 
+        })
+    }
+    
 
 }
