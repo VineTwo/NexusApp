f1924df8a6b73df14ec93cd474a78e80f29499be
nickpotts3 <32562264+nickpotts3@users.noreply.github.com>
2018-01-12 17:19:00 -0500

    Got sign in validation to work

--- Nexus/LoginViewController.swift
+++ Nexus/LoginViewController.swift
@@ -28,20 +28,31 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        emailTextField.backgroundColor = textFieldColor
         emailTextField.tintColor = UIColor.lightText
         emailTextField.textColor = UIColor.black
         emailTextField.attributedPlaceholder = NSAttributedString(string: emailTextField.placeholder!, attributes: [NSAttributedStringKey.foregroundColor: UIColor(white: 1.0, alpha: 1.0)])
+        let bottomLayer = CALayer()
+        bottomLayer.frame = CGRect(x: 0, y: 33, width: 335.53, height: 0.6)
+        bottomLayer.backgroundColor = UIColor.white.cgColor
+        emailTextField.layer.addSublayer(bottomLayer)
         
-        passwordTextField.backgroundColor = textFieldColor
-        passwordTextField.tintColor = UIColor.gray
+        
+        passwordTextField.tintColor = UIColor.lightText
         passwordTextField.textColor = UIColor.black
-        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSAttributedStringKey.foregroundColor: UIColor(white: 1.0, alpha: 0.9)])
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSAttributedStringKey.foregroundColor: UIColor(white: 1.0, alpha: 1.0)])
+        let bottomLayerPassword = CALayer()
+        bottomLayerPassword.frame = CGRect(x: 0, y: 33, width: 335.53, height: 0.6)
+        bottomLayerPassword.backgroundColor = UIColor.white.cgColor
+        passwordTextField.layer.addSublayer(bottomLayerPassword)
+        
+        loginButton.layer.cornerRadius = 10.0
         loginButton.isEnabled = false
         loginButton.setTitleColor(UIColor.lightText, for: UIControlState.normal)
-        handleTextField()
         loginButton.backgroundColor = UIColor.clear
+        
+        handleTextField()
     }
+    
     func handleTextField() {
         emailTextField.addTarget(self, action: #selector(LoginViewController.textFieldDidChange), for: UIControlEvents.editingChanged)
         passwordTextField.addTarget(self, action: #selector(LoginViewController.textFieldDidChange), for: UIControlEvents.editingChanged)
@@ -61,21 +72,10 @@ class LoginViewController: UIViewController {
         Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, Error) in
             if Error != nil {
                 print(Error!.localizedDescription)
-                let errorCode = AuthErrorNameKey
-                    switch errorCode {
-                    case "FIRAuthErrorCodeInvalidEmail":
-                        print("Invalid email, please try again")
-                        self.loginButton.isEnabled = false
-                        break
-                    default:
-                        print("Unknown errors")
-                }
-                return
             }
-            print("No Error")
-
+            else {
+                self.performSegue(withIdentifier: "signInToTabBarVC", sender: nil)
+            }
         })
     }
-    
-
 }
