a5bee6f65df4f63255e645cc28c969eed8c03766
nickpotts3 <32562264+nickpotts3@users.noreply.github.com>
2018-01-25 10:47:26 -0500

    Generate a contact QR code based on user input

--- Nexus/ContactCodeViewController.swift
+++ Nexus/ContactCodeViewController.swift
@@ -11,13 +11,34 @@ import UIKit
 class ContactCodeViewController: UIViewController {
     @IBOutlet weak var contactImageVIew: UIImageView!
     
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let contactInfo = "MECARD:N:Joe_Morales;TEL:6191029501;EMAIL:first.last@email.com;URL:http://website.com;;"
+        
+   
+        // Do any additional setup after loading the view.
+    }
+  
+    
+    @IBAction func generateButton_TouchUpInside(_ sender: Any) {
+        let name = firstNameTextField.text! + lastNameTextField.text!
+        let phone = phoneNumberTextField.text!
+        let email = emailTextField.text!
+       // "MECARD:N:Joe_Morales;TEL:6191029501;EMAIL:first.last@email.com;URL:http://website.com;;"
+        let contactInfo = "MECARD:N:\(name);TEL:\(phone);EMAIL:\(email)"
         
         let imageURL = URL(string: "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=\(contactInfo)")
-        //userRef.setValue(["instQrURl": contactInfo])
         var image: UIImage?
         if let url = imageURL {
             print("Inside first if let")
@@ -36,9 +57,11 @@ class ContactCodeViewController: UIViewController {
                 }
             }
         }
-        // Do any additional setup after loading the view.
+        
+        
+        
+        
     }
-
-
+    
 
 }
