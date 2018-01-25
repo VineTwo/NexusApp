efbdf93365341f7c534edb470ecf2b22de4fb49b
nickpotts3 <32562264+nickpotts3@users.noreply.github.com>
2018-01-25 10:37:48 -0500

    Got the setup for a Contact QR Code

--- Nexus/ContactCodeViewController.swift
+++ Nexus/ContactCodeViewController.swift
@@ -9,27 +9,36 @@
 import UIKit
 
 class ContactCodeViewController: UIViewController {
-
+    @IBOutlet weak var contactImageVIew: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let contactInfo = "MECARD:N:Joe_Morales;TEL:6191029501;EMAIL:first.last@email.com;URL:http://website.com;;"
+        
+        let imageURL = URL(string: "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=\(contactInfo)")
+        //userRef.setValue(["instQrURl": contactInfo])
+        var image: UIImage?
+        if let url = imageURL {
+            print("Inside first if let")
+            //All network operations has to run on different thread(not on main thread).
+            DispatchQueue.global(qos: .userInitiated).async {
+                let imageData = NSData(contentsOf: url)
+                //All UI operations has to run on main thread.
+                DispatchQueue.main.async {
+                    if imageData != nil {
+                        print("Should see image")
+                        image = UIImage(data: imageData! as Data)!
+                        self.contactImageVIew.image = image
+                        self.contactImageVIew.sizeToFit()
+                    }
+                    
+                }
+            }
+        }
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
