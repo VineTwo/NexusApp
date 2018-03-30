5bef38d7bdc4b066bd9b2bfb2465f52e3c3bf828
nickpotts3 <32562264+nickpotts3@users.noreply.github.com>
2018-03-29 23:25:58 -0400

    Started the design of the business card page

---
+++ Nexus/TabBarVCs/businessCardViewController.swift
@@ -0,0 +1,28 @@
+//
+//  businessCardViewController.swift
+//  Nexus
+//
+//  Created by Nick potts on 3/29/18.
+//  Copyright © 2018 Matthew Foote. All rights reserved.
+//
+
+import UIKit
+
+class businessCardViewController: UIViewController {
+
+    @IBOutlet weak var bottomLayer: UIView!
+    @IBOutlet weak var companyLabel: UILabel!
+    @IBOutlet weak var topLayer: UIView!
+    @IBOutlet weak var nameTextField: UILabel!
+    @IBOutlet weak var jobTextField: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
