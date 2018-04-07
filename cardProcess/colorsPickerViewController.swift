54feb7993c05a296656f0bb643eb661da04c3869
nickpotts3 <32562264+nickpotts3@users.noreply.github.com>
2018-04-06 20:10:59 -0400

    Stores the two colors in UserDefaults

--- cardProcess/colorsPickerViewController.swift
+++ cardProcess/colorsPickerViewController.swift
@@ -15,30 +15,56 @@ class colorsPickerViewController: UIViewController, UIPickerViewDelegate, UIPick
     @IBOutlet weak var secondColorTextField: UITextField!
     
     let colorPicker = UIPickerView()
+    let secondColorPicker = UIPickerView()
     let myPickerData = [String](arrayLiteral: "Blue", "Brown", "Black", "Red", "Green", "Yellow", "Gray",  "Orange", "Purple", "Magenta", "White")
+    let secondPickerData = [String](arrayLiteral: "Blue", "Brown", "Black", "Red", "Green", "Yellow", "Gray",  "Orange", "Purple", "Magenta", "White")
     override func viewDidLoad() {
         super.viewDidLoad()
         firstColorTextField.inputView = colorPicker
+        secondColorTextField.inputView = secondColorPicker
         colorPicker.delegate = self
+        secondColorPicker.delegate = self
+        
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int { return 1}
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int { return myPickerData.count}
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if(firstColorTextField.isFirstResponder) {
+            return myPickerData.count
+        } else {
+            return secondPickerData.count
+        }
+    }
     
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? { return myPickerData[row]}
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        if(secondColorTextField.isFirstResponder) {
+            return secondPickerData[row]
+        } else {
+            return myPickerData[row]
+        }
+        
+        
+    }
     
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) { firstColorTextField.text = myPickerData[row]}
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        firstColorTextField.text = myPickerData[row]
+        secondColorTextField.text = secondPickerData[row]
+    }
 
     
-
+    @IBAction func finishButton_TouchUpInside(_ sender: Any) {
+        UserDefaults.standard.set(firstColorTextField.text, forKey: "firstColor")
+        UserDefaults.standard.set(firstColorTextField.text, forKey: "secondColor")
+    }
+    
   
 
 }
